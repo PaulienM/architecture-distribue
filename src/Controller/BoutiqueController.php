376acc569fa9571e479bc495c602fd3d209d4a02
Paulien <paulien.m.73@gmail.com>
2019-12-02 11:47:44 +0100

    Create boutique routes, actions and views

--- src/Controller/BoutiqueController.php
+++ src/Controller/BoutiqueController.php
@@ -4,12 +4,32 @@
 namespace App\Controller;
 
 
+use App\Service\BoutiqueService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class BoutiqueController extends AbstractController
 {
-    public function index()
+    public function index(BoutiqueService $boutiqueService)
     {
-        return $this->render('boutique/index.html.twig');
+        $categoryList = $boutiqueService->findAllCategories();
+        return $this->render('boutique/index.html.twig', [
+            "categories" => $categoryList
+        ]);
+    }
+
+    public function rayon(BoutiqueService $boutiqueService, int $idCategory)
+    {
+        $products = $boutiqueService->findProduitsByCategorie($idCategory);
+        return $this->render('boutique/rayon.html.twig', [
+            "products" => $products
+        ]);
+    }
+
+    public function search(BoutiqueService $boutiqueService, string $searchText)
+    {
+        $products = $boutiqueService->findProduitsByLibelleOrTexte($searchText);
+        return $this->render('boutique/rayon.html.twig', [
+            "products" => $products
+        ]);
     }
 }
