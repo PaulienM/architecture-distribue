b9b210fd3a6a88fcf97143e0b458aa381b68ed4a
Paulien <paulien.m.73@gmail.com>
2019-12-02 11:48:33 +0100

    Search from navbar with jquery

--- assets/js/app.js
+++ assets/js/app.js
@@ -11,6 +11,12 @@
 require('bootstrap');
 
 // Need jQuery? Install it with "yarn add jquery", then uncomment to require it.
-// const $ = require('jquery');
+const $ = require('jquery');
 
 //console.log('Hello Webpack Encore! Edit me in assets/js/app.js');
+
+$('#searchButton').click(function () {
+    let searchText = $('#searchText').val().trim()
+    location = `/recherche/${encodeURI(searchText)}`;
+    return false;
+});
