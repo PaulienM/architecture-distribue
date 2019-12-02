227453ba73f940e8ca7e1b330dd1a09dc2858545
Paulien <paulien.m.73@gmail.com>
2019-12-02 09:42:06 +0100

    Change route for homepage

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -8,10 +8,8 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class DefaultController extends AbstractController
 {
-    public function index($userName)
+    public function index()
     {
-        return $this->render('default/home.html.twig', [
-            'userName' => $userName
-        ]);
+        return $this->render('default/home.html.twig');
     }
 }
