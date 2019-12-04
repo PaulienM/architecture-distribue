925a360a258dc8c770a2cfbaf718b5f5205be02b
Paulien <paulien.m.73@gmail.com>
2019-12-04 08:39:23 +0100

    Add fontawesome in project

--- assets/js/app.js
+++ assets/js/app.js
@@ -10,6 +10,9 @@
 
 require('bootstrap');
 
+require('@fortawesome/fontawesome-free/css/all.min.css');
+require('@fortawesome/fontawesome-free/js/all.js');
+
 // Need jQuery? Install it with "yarn add jquery", then uncomment to require it.
 const $ = require('jquery');
 
